15e0134f7ea72a295159073e88fcaa92d67b21ff
Dominic H <domhauton@gmail.com>
2017-02-21 20:36:24 +0000

    Actions working

--- src/com/domhauton/cm30229/lejos/action/Action.java
+++
@@ -1,8 +0,0 @@
-package com.domhauton.cm30229.lejos.action;
-
-/**
- * Created by Dominic Hauton on 21/02/17.
- */
-public enum Action {
-  FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT
-}
---
+++ src/com/domhauton/cm30229/lejos/action/actions/Action.java
@@ -0,0 +1,22 @@
+package com.domhauton.cm30229.lejos.action.actions;
+
+/**
+ * Created by Dominic Hauton on 21/02/17.
+ */
+public enum Action {
+  FORWARD(new MoveForward()),
+  BACKWARD(null),
+  ROTATE_LEFT(null),
+  ROTATE_RIGHT(null),
+  IDLE(new Idle());
+
+  private ExecutableAction executableAction;
+
+  Action(ExecutableAction executableAction) {
+    this.executableAction = executableAction;
+  }
+
+  public ExecutableAction getExecutableAction() {
+    return executableAction;
+  }
+}
